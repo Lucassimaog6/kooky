65848c26a6a49b6cfebd0a9eda34f2e9319bdf77
Lucas Ferreira Simão <72400528@aluno.faculdadecotemig.br>
2024-03-20 16:39:44 -0300

    Adiciona componente Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+    return (
+        <div className="navbar bg-base-100">
+            <div className="navbar-start"></div>
+            <div className="navbar-center">
+                <a className="btn btn-ghost text-xl">Kooky</a>
+            </div>
+            <div className="navbar-end"></div>
+        </div>
+    );
+}
