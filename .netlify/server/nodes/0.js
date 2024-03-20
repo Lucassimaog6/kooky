e0a58f2d8d5ee5978190b5c6df7b28e49e0af5ae
Lucas Ferreira Simão <72400528@aluno.faculdadecotemig.br>
2024-03-20 16:38:08 -0300

    delete old project for remake

--- .netlify/server/nodes/0.js
+++
@@ -1,7 +0,0 @@
-
-
-export const index = 0;
-export const component = async () => (await import('../entries/fallbacks/layout.svelte.js')).default;
-export const file = '_app/immutable/components/layout.svelte-5d92cd60.js';
-export const imports = ["_app/immutable/components/layout.svelte-5d92cd60.js","_app/immutable/chunks/index-521e50df.js"];
-export const stylesheets = [];
--- .netlify/server/nodes/1.js
+++
@@ -1,7 +0,0 @@
-
-
-export const index = 1;
-export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const file = '_app/immutable/components/error.svelte-9c153a6c.js';
-export const imports = ["_app/immutable/components/error.svelte-9c153a6c.js","_app/immutable/chunks/index-521e50df.js","_app/immutable/chunks/singletons-7d617ba9.js"];
-export const stylesheets = [];
