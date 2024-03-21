efbcdd4be4801511f422d012860f081951ee9c7b
Lucas Ferreira Simão <72400528@aluno.faculdadecotemig.br>
2024-03-21 11:33:48 -0300

    Adicionado componente Footer ao projeto

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+            <aside>
+                <p>Feito com amor e carinho por <a className="link hover:link-primary" href="https://github.com/Lucassimaog6">Lucas Ferreira Simão</a></p>
+            </aside>
+        </footer>
+    );
+}
