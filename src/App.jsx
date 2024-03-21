0cce0bf340f63d19a6528332825dd615a721265d
Lucas Ferreira Simão <72400528@aluno.faculdadecotemig.br>
2024-03-21 12:00:45 -0300

    Atualizações na renderização de imagens na página principal

--- src/App.jsx
+++ src/App.jsx
@@ -13,16 +13,13 @@ export default function App() {
         listAll(listRef).then((list) => {
             list.items.forEach((item) => {
                 addUrlToList(item);
-                console.log("useEffect")
             })
         })
     }, [])
 
     const addUrlToList = (ref) => {
         getDownloadURL(ref).then((downloadURL) => {
-            setListUrls([...listUrls, downloadURL])
-            console.log("async")
-            console.log(listUrls)
+            setListUrls(old => [...old, downloadURL])
         })
     }
 
@@ -32,7 +29,7 @@ export default function App() {
             <div className="w-screen flex justify-center">
                 <FileInput addUrlToList={addUrlToList} />
             </div>
-            <main className="mx-auto my-4 w-fit grid gap-4 place-items-center grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3">
+            <main className="mx-auto my-4 p-8 w-fit grid gap-4 place-items-center grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3">
                 {listUrls.map((url, index) => (
                     <img className="rounded-lg w-[90%]" src={url} key={index} />
                 ))}
